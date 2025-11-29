245a44e90c997991145c53764719d44dac0996cf
Dzhekishev <djekishev03@gmail.com>
2025-11-29 16:28:09 +0600

    deploy

--- edusphere/edusphere/asgi.py
+++ edusphere/edusphere/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusphere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusphere.edusphere.settings')
 
 application = get_asgi_application()
